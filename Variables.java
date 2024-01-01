8cf33ba1db531330b2b0e1bceaad2382de18f18d
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-01 22:33:22 +0530

    1st January

---
+++ Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main (String[] args ) {
+        int a = 10 ;
+        System.out.println ("The value of a is : " + a);
+
+        float b = 1.255f;
+        System.out.println("The valuse of float is : " + b);
+
+        char ch = 'A';
+        System.out.println("The character is : " + ch);
+
+        String d = "UJJWAL KUMAR ";
+        System.out.println("The string is : "+ d);
+
+        
+
+        
+
+
+    }
+}
