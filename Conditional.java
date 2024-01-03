2f50ceb065977a1dbb5cbefd9866d18a34803279
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-03 23:08:30 +0530

    conditional

---
+++ Conditional.java
@@ -0,0 +1,13 @@
+public class Conditional {
+    public static void main(String args[]) {
+        int age = 10;
+        if(age>18) {
+            System.out.println("Can Vote");
+        }
+        else{
+            System.out.println("Can't Vote");
+        }
+
+    }
+    
+}
