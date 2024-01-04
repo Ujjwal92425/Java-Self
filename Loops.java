32ec6c353401b0c5395809dcc5be416017ce9d89
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-04 23:33:15 +0530

    for loop
    loop

---
+++ Loops.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+        int i=0;
+        for (i = 0; i <=number; i++) {
+            System.out.println("******");
+        }
+        
+        input.close();
+    } 
+}
