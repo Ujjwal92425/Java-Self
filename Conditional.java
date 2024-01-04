e3b61089efe67299eae91ec2697ddcc051200dde
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-04 23:04:42 +0530

    conditional

--- Conditional.java
+++ Conditional.java
@@ -1,11 +1,24 @@
+// public class Conditional {
+//     public static void main(String args[]) {
+//         int age = 10;
+//         if(age>18) {
+//             System.out.println("Can Vote");
+//         }
+//         else{
+//             System.out.println("Can't Vote");
+//         }
+
+//     }
+    
+// }
 public class Conditional {
     public static void main(String args[]) {
-        int age = 10;
-        if(age>18) {
-            System.out.println("Can Vote");
+        int a = 10;
+        if(a>0) {
+            System.out.println("Positive");
         }
         else{
-            System.out.println("Can't Vote");
+            System.out.println("Negative");
         }
 
     }
