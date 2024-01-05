685afb98433c08b42d5c0f856fb565e636e774b5
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-05 22:20:50 +0530

    table printing
    table

--- Loops.java
+++ Loops.java
@@ -2,14 +2,28 @@ import java.util.*;
 
 public class Loops {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+       /*  Scanner sc = new Scanner(System.in);
 
-        int number = input.nextInt();
+        int number = sc.nextInt();
         int i=0;
         for (i = 0; i <=number; i++) {
             System.out.println("******");
         }
         
-        input.close();
+        sc.close();
+        */
+
+
+        // Table printing : Using loop 
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N*i);
+            
+        }
+        sc.close();
     } 
 }
+
