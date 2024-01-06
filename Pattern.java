61e37f82200b743602d156bc7a1a4f0f26572cf5
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-06 17:03:30 +0530

    Pattern Printing
    Pattern printing of  different  style using star symbols

---
+++ Pattern.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+       
+
+    //    Pattern 1 : *
+    //                **
+    //                ***
+    //                ****
+        Scanner num = new Scanner(System.in);
+
+        int a= num.nextInt();
+        num.close();                                          
+        // for (int i = 1; i<= a; i++) {                         
+        //     for (int j = 1; j <= i ; j++) {                   
+        //         System.out.print("*");                        
+        //     }
+        //     System.out.println("");    
+        // }  
+           
+
+
+        // Pattern 2 : ****
+        //             ***
+        //             **
+        //             *
+
+
+        for (int i = 1; i <=a ; i++) {
+            for (int j = 1; j <= a-i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");     
+        }
+
+
+
+
+        
+    }
+    
+}
