a3d843f4724b55bb10c830c6556ca14cdfe53193
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-06 17:11:35 +0530

    Pattern print part 2

--- Pattern.java
+++ Pattern.java
@@ -26,13 +26,14 @@ public class Pattern {
         //             **
         //             *
 
-
+        /*
         for (int i = 1; i <=a ; i++) {
             for (int j = 1; j <= a-i+1; j++) {
                 System.out.print("*");
             }
             System.out.println("");     
         }
+        */
 
 
 
