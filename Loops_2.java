667c7ec76a07721b6b1f6e2db064b91d3f3efae6
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-07 22:13:35 +0530

    Factorial
    Factorial of any number including 0 and 1 .

---
+++ Loops_2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Loops_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the no. whose factroial u want : ");
+        int num = input.nextInt();
+        input.close();
+        
+
+        if (num == 0 || num==1) {
+            System.out.println("Fatorial of "+num+ " is : 1");  
+        }
+        else {
+            int factorial=1;
+            for (int i = 1; i <= num; i++) {
+                factorial=factorial*i; 
+            }
+            System.out.println("Factorial of "+num+ " is  : "+factorial);
+        }
+    }
+    
+}
