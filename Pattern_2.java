aaa00785619700095e92073b092515efce95a9d2
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-08 18:50:16 +0530

    Hollow Rectangle

--- Pattern_2.java
+++ Pattern_2.java
@@ -1,16 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Pattern_2 {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        int a = input.nextInt();
-        input.close();
+        
+        // int a = 5;
+        
+        // for (int i = 1; i <= a; i++) {
+        //     for (int j = 1; j <=1; j++) {
+        //         System.out.print("*");
+        //     }
+               
+        // }
 
-        for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= a+1; j++) {
-                System.out.print("*");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns:");
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == rows || j == 1 || j == columns)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
             }
-            System.out.println();   
+            System.out.println();
         }
 
     }
