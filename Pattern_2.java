cac5723b8409f74f07069f9f0cbac22c2d2bbbd3
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-08 18:38:13 +0530

    Advance Pattern

--- Pattern_2.java
+++ Pattern_2.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class Pattern_2 {
-    
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int a = input.nextInt();
+        input.close();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();   
+        }
+
+    }
 }
