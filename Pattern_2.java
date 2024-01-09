c2cbc1cc71c55f7b8c09f9a68c339cfe1d6d0f05
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-01-09 17:53:36 +0530

    update

--- Pattern_2.java
+++ Pattern_2.java
@@ -16,6 +16,8 @@ public class Pattern_2 {
         System.out.println("Enter the number of rows and columns:");
         int rows = sc.nextInt();
         int columns = sc.nextInt();
+        sc.close();
+
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= columns; j++) {
                 if (i == 1 || i == rows || j == 1 || j == columns)
