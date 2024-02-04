a55dc4fab45f99d9d1a8c9ca9b85b8cde8f8bc63
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-02-04 19:26:47 +0530

    4 feb

--- Function_2.java
+++ Function_2.java
@@ -1,3 +1,10 @@
 public class Function_2 {
+    public static void main(String args[]) {
+        double d =7.;
+        double a = .7;
+        double b = 7;
+        System.out.println(a+" "+ d + " "+ b);
+
+    }
     
 }
