39e39776934613d1ba52d36f4e5ae7ed65a1f844
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-04-22 15:48:32 +0530

    buffer

---
+++ BufferReader.java
@@ -0,0 +1,20 @@
+// import java.io.IOException;
+// import java.io.InputStream;
+// import java.io.InputStreamReader;
+// import java.net.SocketPermission;
+// import java.io.*;
+
+// public class BufferReader {
+//     public static void main(String[] args) throws IOException {
+//         InputStreamReader r = new InputStreamReader(System.in);
+//         BufferReader br = new BufferedReader(r);
+//         System.out.println("Enter ur name");
+//         String name = br.readLine();
+//         System.out.println("Welcome "+name);
+//     }
+// }
+
+
+
+
+
